a8c26c8f22d8e9e5330b9dc0d33a046a136db592
Windree <windree@dragonsoul.com>
2024-07-11 07:24:55 +0000

    feat: run specific code on demand

--- app.sh
+++ app.sh
@@ -1,55 +1,12 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-function check_source() {
-    if [ -d "$1" ]; then
-        return 0
-    fi
-    echo >&2 "Source directory '$1' not found"
-    exit 1
-}
-
-function check_target() {
-    if [ -d "$1" ]; then
-        return 0
-    fi
-    echo >&2 "Target directory '$1' not found"
-    exit 2
-}
-
-function get_docker_status() {
-    if [ ! -f "$1" ]; then
-        echo ""
-    elif [ -z "$(docker compose -f "$1" top)" ]; then
-        echo "stopped"
-    else
-        echo "running"
-    fi
-}
-
-check_source "${2:-}"
-check_target "${1:-}"
-
-docker_compose=$2/docker-compose.yml
-docker_status=$(get_docker_status "$docker_compose")
+docker_compose=
+docker_status=
 dir=$(dirname "$(readlink -f -- "$0")")
 dar_image=$(basename "$dir")-dar
 temp=$(basename "$(mktemp --dry-run)")
-log=$(mktemp)
-
-function cleanup() {
-    if [ "$docker_status" == "running" ]; then
-        start_docker "$docker_compose"
-    fi
-    if [ -d "$temp" ]; then
-        rm -rf "$temp"
-    fi
-    if [ -f "$log" ]; then
-        rm -f "$log"
-    fi
-}
-
-trap cleanup exit
+log=$(mktemp --dry-run)
 
 function build_image() {
     local dar_image_path=$dir/dar/image
@@ -61,16 +18,27 @@ function build_image() {
     fi
 }
 
-function main() {
-    local backup_path=$1
+function create() {
+    if [ ! -v 1 ] || [ ! -d "$1" ]; then
+        echo "Archive directory '$1' not found"
+    fi
+    if [ ! -v 2 ] || [ ! -d "$2" ]; then
+        echo "Source directory '$2' not found"
+    fi
+
+    local archive_path=$1
     local source_path=$2
     shift 2
+    
+    export docker_compose=$source_path/docker-compose.yml
+    export docker_status=$(get_docker_status "$docker_compose")
+
     if [ "$docker_status" == "running" ]; then
         echo "Stoping docker..."
         stop_docker "$source_path"
     fi
 
-    docker run --rm -v "$source_path:/source" -v "$backup_path:/data" "$dar_image" create "$temp" "$@"
+    docker run --rm -v "$source_path:/source" -v "$archive_path:/data" "$dar_image" create "$temp" "$@"
 
     if [ "$docker_status" == "running" ]; then
         echo "Staring docker..."
@@ -78,12 +46,38 @@ function main() {
     fi
     docker_status=""
 
-    if ! docker run --rm -v "$backup_path/$temp:/data" "$dar_image" test; then
+    if ! docker run --rm -v "$archive_path/$temp:/data" "$dar_image" test; then
         exit 1
     fi
-    mv "$backup_path/$temp/"* "$backup_path"
-    rm -d "$backup_path/$temp/"
+    mv "$archive_path/$temp/"* "$archive_path"
+    rm -d "$archive_path/$temp/"
     echo "Result: OK."
+}
+
+function extract() {
+    if [ ! -v 1 ] || [ ! -d "$1" ]; then
+        echo "Archive directory '$1' not found"
+    fi
+    if [ ! -v 2 ] || [ ! -d "$2" ]; then
+        echo "Target directory '$2' not found"
+    fi
+
+    local archive_path=$1
+    local target_path=$2
+    shift 2
+
+    docker run --rm -v "$target_path:/target" -v "$archive_path:/data" "$dar_image" extract "$@"
+    echo "Result: OK."
+}
+
+function get_docker_status() {
+    if [ ! -f "$1" ]; then
+        echo ""
+    elif [ -z "$(docker compose -f "$1" top)" ]; then
+        echo "stopped"
+    else
+        echo "running"
+    fi
 }
 
 function start_docker() {
@@ -100,5 +94,36 @@ function stop_docker() {
     fi
 }
 
+
+function cleanup() {
+    if [ "$docker_status" == "running" ] && [ -n "$docker_compose" ]; then
+        start_docker "$docker_compose"
+    fi
+    if [ -d "$temp" ]; then
+        rm -rf "$temp"
+    fi
+    if [ -f "$log" ]; then
+        rm -f "$log"
+    fi
+}
+
+trap cleanup exit
+
+action=$1
+shift
+
 build_image
-main "$@"
+
+case "$action" in
+"create")
+    create "$@"
+    ;;
+"extract")
+    extract "$@"
+    ;;
+*)
+    echo "Unsupported action '$action'"
+    exit -2
+    ;;
+
+esac
